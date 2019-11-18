b280e70586b0c80a8435ab512c44c619caec23a2
George <gwong323@yahoo.com>
2019-11-18 11:34:36 +0000

    start 2111 teach

---
+++ 2111teach.js
@@ -0,0 +1,39 @@
+// shortcuts:
+
+// cut: ctrl x
+// copy: ctrl c
+// paste: ctrl v
+// undo: ctrl z
+// redo: shift+ctrl x
+// paste multiple lines: ctrl c then ctrl v on same line
+// highlight and move text
+// save file: ctrl s
+// select next: ctrl d
+
+// comment: ctrl /
+
+// Why use comment?
+// Why comment out code?
+
+// Why use pairs () {} "" ?
+
+// Christmas Tree Code
+
+function setup() {
+  createCanvas(1920, 1080);
+}
+
+function draw() {
+  background(220);
+}
+
+// Race Game
+// Show project, break down if statements
+
+// setup to draw background and show characters
+//
+
+// start game after 5 seconds, show counter
+// show two scores and game countdown timer, 60 seconds
+// if s show L sprite and if D show right sprite. Hurdle X - 1
+// if A or F show jump sprite and y + 10 for one second
