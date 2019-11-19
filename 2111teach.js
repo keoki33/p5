386fe63ba942c846b75a53eadc042ff0b243563e
George <gwong323@yahoo.com>
2019-11-19 17:00:46 +0000

    2111 game added, lesson finished

--- 2111teach.js
+++ 2111teach.js
@@ -1,4 +1,4 @@
-// shortcuts:
+// teach shortcuts:
 
 // cut: ctrl x
 // copy: ctrl c
@@ -11,6 +11,7 @@
 // select next: ctrl d
 // comment: ctrl /
 // Why use pairs () {} "" ?
+// tidy code: shift + tab
 
 // Why use comment?
 // Why comment out code?
@@ -19,6 +20,11 @@
 
 let snowflakes = []; // array to hold snowflake objects
 let lights = [];
+// let img
+
+// function preload() {
+//   img = loadImage('tree.jpeg');
+// }
 
 function setup() {
   createCanvas(800, 550);
@@ -122,14 +128,16 @@ function snowflake() {
   };
 }
 
-function light() {
-  this.y = random(100, 500);
-  this.x = random(380 - this.y / 5, 440 + this.y / 5);
-  this.d = random(25);
+class light {
+  constructor() {
+    this.y = random(100, 500);
+    this.x = random(380 - this.y / 5, 440 + this.y / 5);
+    this.d = random(25);
+  }
 
-  this.display = function() {
+  display() {
     circle(this.x, this.y, this.d);
-  };
+  }
 }
 
 //// ****** CHALLENGE 1 ******** //////
@@ -140,20 +148,206 @@ function light() {
 
 // super challenge:
 
-// Change tree shape, add texture
+// Change tree shape, add star on top
 // make snow bigger, change speed of snowfall
 
 // super duper challenge:
 
 // make you own tree and decorations with array and class
 
-// Race Game
-// Show project, break down if statements
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 
-// setup to draw background and show characters
-//
+// two player shoot game
 
-// start game after 5 seconds, show counter
-// show two scores and game countdown timer, 60 seconds
-// if s show L sprite and if D show right sprite. Hurdle X - 1
-// if A or F show jump sprite and y + 10 for one second
+// variables for p1 score, p2 score, p1 y, p2 y, array of p1 bullets, array p2 bullets
+// draw triangle on left and right side. y changes, x always stays same
+// controls:  p1 q:up, a:down  , z:shoot ----- p2 p:up, l:down, ,:shoot
+// bullets class
+// when shoot button pressed, create new bullet in corresponding array, update, and draw
+// if p1 bullets hit p2, p1 score ++1 and vice versa
+
+////  // ********* CHALLENGE *******   /////////
+
+// Make right player change color when hit
+
+// What is a class? Why use? How use?
+// What is an array? What is javascript object?
+// What are the arrays in below code holding?
+
+//// Insane Challenge //////////
+
+/// Change code to allow both players to also move left and right.
+
+// Make your own game, copy parts of code if you need to. The most important part is understanding // what the code does.
+
+//////////// UNCOMMENT GAME CODE BELOW ///////////////
+//////////// UNCOMMENT GAME CODE BELOW ///////////////
+//////////// UNCOMMENT GAME CODE BELOW ///////////////
+//////////// UNCOMMENT GAME CODE BELOW ///////////////
+
+// let p1Score = 0
+// let p2Score = 0
+// let p1Y = 250
+// let p2Y = 250
+// let playerSpeed = 20
+// let p1Bullets = []
+// let p2Bullets = []
+// let bulletSpeed = 20
+// let p1Hit = false
+// let p2Hit = false
+// let instructions = true
+
+// function setup() {
+//   createCanvas(800, 600);
+//   noStroke();
+// }
+
+// function draw() {
+//   fill("black")
+//   background(220);
+//   if (instructions) {
+//     displayInstructions()
+//   }
+//   displayScore();
+//   if (p1Hit) {
+//     fill("red")
+//   } else {
+//     fill("white")
+//   }
+//   triangle(5, 40 + p1Y, 5, 80 + p1Y, 35, p1Y + 60)
+//   p1Hit = false
+//   fill("white")
+//   triangle(795, 40 + p2Y, 795, 80 + p2Y, 760, 60 + p2Y)
+
+//   // handleKey()
+//   for (let bullet of p1Bullets) {
+//     bullet.p1Shoot();
+//     bullet.display();
+
+//   }
+//   for (let bullet of p2Bullets) {
+//     bullet.p2Shoot()
+//     bullet.display();
+//   }
+
+// }
+
+// function displayScore() {
+//   textSize(20);
+//   text(`score: ${p1Score}`, 10, 30);
+//   text(`score: ${p2Score}`, 700, 30);
+// }
+
+// function displayInstructions() {
+//   textSize(20);
+//   text("CONTROLS:", 300, 200)
+//   text("Left Player:", 150, 250)
+//   text("q : move up", 150, 300)
+//   text("a : move down", 150, 330)
+//   text("z : pew pew", 150, 360)
+
+//   text("Right Player:", 500, 250)
+//   text("p : move up", 500, 300)
+//   text("l : move down", 500, 330)
+//   text(", : pew pew", 500, 360)
+
+// }
+
+// // function handleKey() {
+// //   if (keyIsDown(81)) {
+// //     p1Y = p1Y - playerSpeed
+// //   } else if (keyIsDown(65)) {
+// //     {
+// //       p1Y = p1Y + playerSpeed
+// //     }
+// //   } else if (keyIsDown(90)) {
+// //     {
+// //       p1Bullets.push(new bullet(35, 60 + p1Y))
+// //     }
+// //   } else if (keyIsDown(80)) {
+// //     {
+// //       p2Y = p2Y - playerSpeed
+// //     }
+// //   } else if (keyIsDown(76)) {
+// //     {
+// //       p2Y = p2Y + playerSpeed
+// //     }
+// //   } else if (keyIsDown(188)) {
+// //     {
+// //       p2Bullets.push(new bullet(760, 60 + p2Y))
+// //     }
+// //   }
+// // }
+
+// function keyPressed() {
+//   if (keyCode === 81) {
+//     p1Y = p1Y - playerSpeed
+//   } else if (keyCode === 65) {
+//     p1Y = p1Y + playerSpeed
+
+//   } else if (keyCode === 90) {
+//     {
+//       instructions = false
+//       p1Bullets.push(new bullet(35, 60 + p1Y))
+//     }
+//   } else if (keyCode === 80) {
+//     {
+//       p2Y = p2Y - playerSpeed
+//     }
+//   } else if (keyCode === 76) {
+//     {
+//       p2Y = p2Y + playerSpeed
+//     }
+//   } else if (keyCode === 188) {
+//     {
+
+//       instructions = false
+//       p2Bullets.push(new bullet(760, 60 + p2Y))
+//     }
+//   }
+// }
+
+// function keyTyped() {
+//   if (key === "q") {
+//     p1Y = p1Y - playerSpeed
+//   } else if (key === "a") {
+//     p1Y = p1Y + playerSpeed
+//   }
+// }
+
+// class bullet {
+//   constructor(x, y) {
+//     this.y = y
+//     this.x = x
+//     this.d = random(25)
+//   }
+
+//   display() {
+//     circle(this.x, this.y, this.d)
+//   }
+
+//   p1Shoot() {
+//     this.x = this.x + 1 * bulletSpeed
+//     let d = dist(this.x, this.y, 760, 60 + p2Y)
+//     if (d < 15) {
+//       p1Score = p1Score + 1
+
+//     }
+//   }
+
+//   p2Shoot() {
+//     // p1Hit=false
+//     this.x = this.x - 1 * bulletSpeed
+//     let d = dist(this.x, this.y, 35, 60 + p1Y)
+//     if (d < 15) {
+//       p2Score = p2Score + 1
+//       p1Hit = true
+//     }
+//   }
+
+// }
